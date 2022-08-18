dc89e07a59e50dfb84fec392d8176f1b0e866ba2
doniaskima <doniaskima344@gmail.com>
2022-08-18 21:17:39 +0100

    clear

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,49 +1,52 @@
-import React, { useState, useEffect } from 'react'
-import useStyles from "./styles";
+import React, { useState, useEffect } from 'react';
 import { TextField, Button, Typography, Paper } from '@material-ui/core';
+import { useDispatch, useSelector } from 'react-redux';
 import FileBase from 'react-file-base64';
-import { useDispatch } from 'react-redux';
-import { createPost,updatePost } from '../../actions/posts';
-import { useSelector } from 'react-redux';
 
-const Form = ({currentId, setCurrentId}) => {
-    const [postData, setPostData] = useState({
-        creator: '', title: '', message: '', tags: '', selectedFile: ''
-    })
-    const post = useSelector((state) => (currentId ? state.posts.find((message) => message._id === currentId) : null));
+import useStyles from './styles';
+import { createPost, updatePost } from '../../actions/posts';
 
+const Form = ({ currentId, setCurrentId }) => {
+    const [postData, setPostData] = useState({ creator: '', title: '', message: '', tags: '', selectedFile: '' });
+    const post = useSelector((state) => (currentId ? state.posts.find((message) => message._id === currentId) : null)); // only find that specific post
+    const dispatch = useDispatch();
     const classes = useStyles();
-    const dispatch = useDispatch(); // dispatch actions
 
     useEffect(() => {
         if (post) setPostData(post);
     }, [post]);
 
-    const handleSubmit = (e) => {
-         e.preventDefault(); // not to get refresh in the browser
-        if(currentId){
-            dispatch(updatePost(currentId,postData));
-        }else {
-              dispatch(createPost(postData));
-        }
-    }
     const clear = () => {
+        setCurrentId(0);
+        setPostData({ creator: '', title: '', message: '', tags: '', selectedFile: '' });
+    };
 
-    }
+    const handleSubmit = async (e) => {
+        e.preventDefault(); // not to get refresh in the browser
+
+        if (currentId === 0) {
+            dispatch(createPost(postData));
+            clear();
+        } else {
+            dispatch(updatePost(currentId, postData));
+            clear();
+        }
+    };
+
     return (
         <Paper className={classes.paper}>
-            <form className="off" noValidate className={classes.form} onSubmit={handleSubmit}>
-                <Typography variant="h6">{currentId ? `Editing "${post.title}"` : 'Creating notes'}</Typography>
-                <TextField name="creator" variant="outlined" label="creator" fullWidth value={postData.creator} onChange={(e) => setPostData({ ...postData, creator: e.target.value })} />
-                <TextField name="title" variant="outlined" label="title" fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })} />
-                <TextField name="message" variant="outlined" label="message" fullWidth value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value })} />
-                <TextField name="tags" variant="outlined" label="tags" fullWidth value={postData.tags} onChange={(e) => setPostData({ ...postData, tags: e.target.value })} />
+            <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
+                <Typography variant="h6">{currentId ? `Editing "${post.title}"` : 'Creating Notes'}</Typography>
+                <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.creator} onChange={(e) => setPostData({ ...postData, creator: e.target.value })} />
+                <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })} />
+                <TextField name="message" variant="outlined" label="Message" fullWidth multiline rows={4} value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value })} />
+                <TextField name="tags" variant="outlined" label="Tags (coma separated)" fullWidth value={postData.tags} onChange={(e) => setPostData({ ...postData, tags: e.target.value.split(',') })} />
                 <div className={classes.fileInput}><FileBase type="file" multiple={false} onDone={({ base64 }) => setPostData({ ...postData, selectedFile: base64 })} /></div>
                 <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth>Submit</Button>
                 <Button variant="contained" color="secondary" size="small" onClick={clear} fullWidth>Clear</Button>
             </form>
         </Paper>
-    )
-}
+    );
+};
 
-export default Form
+export default Form;
--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,7 +1,7 @@
 export default (posts = [], action) => {
     switch (action.type) {
         case "UPDATE":
-            posts.map((post) => (post._id === action.payload._id ? action.payload : post));// action.payload is the updatedPost
+          return  posts.map((post) => (post._id === action.payload._id ? action.payload : post));// action.payload is the updatedPost
         case "FETCH_ALL":
             return action.payload;
         case "CREATE":
