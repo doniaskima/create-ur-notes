ea6b7f1224eafd676281596973cdba66b829b0e2
doniaskima <doniaskima344@gmail.com>
2022-08-19 21:11:04 +0100

    implement google auth

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,13 +1,17 @@
 import React, { useState } from 'react'
 import { Avatar, Button, Paper, Grid, Typography, Container, TextField } from '@material-ui/core';
 import useStyles from "./styles";
+import { useDispatch } from 'react-redux';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import { GoogleLogin } from 'react-google-login';
+import Icon from './Icon';
 import Input from './Input';
 const Auth = () => {
     const classes = useStyles();
     const state = null;
     const [showPassword, setShowPassword] = useState(false);
-    const isSignup = true;
+    const [isSignup, setIsSignup] = useState(false);
+    const dispatch = useDispatch();
     const handleChange = () => {
 
     }
@@ -18,8 +22,20 @@ const Auth = () => {
 
 
     const switchMode = () => {
-
+        setIsSignup((prevIsSignup) => !prevIsSignup)
+        handleShowPassword(false);
     }
+    const googleSuccess = async (res) => {
+        // the question mark dot is a special operator that's not going to throw an error if we don't have access to the rest object
+        const result = res?.profileObj; // we don't gonna have an error if res is not exist
+        const token = res?.tokenId;
+        try {
+            dispatch({ type: "Auth", data: { result, token } });
+        } catch (error) {
+
+        }
+    }
+    const googleError = () => alert('Google Sign In was unsuccessful. Try again later');
     return (
         <Container component="main" maxWidth="xs">
             <Paper className={classes.paper} elevation={3}>
@@ -44,9 +60,22 @@ const Auth = () => {
                         <Input name="password" label="Password" handleChange={handleChange} type={showPassword ? 'text' : 'password'} handleShowPassword={handleShowPassword} />
                         {isSignup && <Input name="confirmPassword" label="Repeat Password" handleChange={handleChange} type="password" />}
                     </Grid>
+
                     <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>
                         {isSignup ? 'Sign Up' : 'Sign In'}
                     </Button>
+                    <GoogleLogin
+                        clientId="1054706857432-6bjo7bs1isu234gbdm5fc4h343jb760s.apps.googleusercontent.com"
+                        //  how we are going to show our button what is going to look
+                        render={(renderProps) => (
+                            <Button className={classes.googleButton} color="primary" fullWidth onClick={renderProps.onClick} disabled={renderProps.disabled} startIcon={<Icon />} variant="contained">
+                                Google Sign In
+                            </Button>
+                        )}
+                        onSuccess={googleSuccess}
+                        onFailure={googleError}
+                        cookiePolicy="single_host_origin"
+                    />
                     <Grid container justify="flex-end">
                         <Grid item>
                             <Button onClick={switchMode}>
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,29 +1,31 @@
 import React from 'react'
-import { AppBar,Avatar,Button,Toolbar, Typography } from '@material-ui/core';
+import { AppBar, Avatar, Button, Toolbar, Typography } from '@material-ui/core';
 import { Link } from "react-router-dom";
 
 import useStyles from './styles';
 const Navbar = () => {
     const classes = useStyles();
     const user = null;
-  return (
-      <AppBar className={classes.appBar} position="static" color="inherit">
-          <Typography className={classes.heading} variant="h2" align="center">Create Your Notes</Typography>
-          <img className={classes.image} src="https://raw.githubusercontent.com/doniaskima/challenge-05-websites-with-reactjs/let's-code-website/src/images/hero-girl.png" alt="coding" height="100" width="200" />
-          <Toolbar className={classes.toolbar}>
-          
-              {user ? (
-                  <div className={classes.profile}>
-                      <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}>{user.result.name.charAt(0)}</Avatar>
-                      <Typography className={classes.userName} variant="h6">{user.result.name}</Typography>
-                      <Button variant="contained" classNaame={classes.logout} color="seconary">Logout</Button>
-                  </div>
-              ): (
-                      <Button component={Link} to="/auth" variant="contained" color="primary">Sign In</Button>  
-          )}
-          </Toolbar>
-      </AppBar>
-  )
+    return (
+        <AppBar className={classes.appBar} position="static" color="inherit">
+            <div className={classes.brandContainer}>
+                <Typography className={classes.heading} variant="h2" align="center">Create Your Notes</Typography>
+                <img className={classes.image} src="https://raw.githubusercontent.com/doniaskima/challenge-05-websites-with-reactjs/let's-code-website/src/images/hero-girl.png" alt="coding" height="100" width="200" />
+            </div>
+            <Toolbar className={classes.toolbar}>
+
+                {user ? (
+                    <div className={classes.profile}>
+                        <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}>{user.result.name.charAt(0)}</Avatar>
+                        <Typography className={classes.userName} variant="h6">{user.result.name}</Typography>
+                        <Button variant="contained" classNaame={classes.logout} color="seconary">Logout</Button>
+                    </div>
+                ) : (
+                    <Button component={Link} to="/auth" variant="contained" color="primary">Sign In</Button>
+                )}
+            </Toolbar>
+        </AppBar>
+    )
 }
 
 export default Navbar
